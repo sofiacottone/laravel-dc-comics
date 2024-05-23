4f258990e9f9d9c72abf9ba0205dece1bca78076
sofiacottone <cottonesofia@outlook.it>
2024-05-23 15:39:52 +0200

    feat: add seeder to populate db

---
+++ database/seeders/ComicsTableSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Comic;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ComicsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {
+        $comics = config('comics');
+
+        foreach ($comics as $comic) {
+            // create istance 
+            $newComic = new Comic();
+
+            // populate attributes
+            $newComic->title = $comic['title'];
+            $newComic->description = $comic['description'];
+            $newComic->thumb = $comic['thumb'];
+            $newComic->price = $comic['price'];
+            $newComic->series = $comic['series'];
+            $newComic->sale_date = $comic['sale_date'];
+            $newComic->type = $comic['type'];
+
+            // save
+            $newComic->save();
+        }
+    }
+}
