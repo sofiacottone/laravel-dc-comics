bc04b77d4cccf4499d86a9f16b82dcccc887e084
sofiacottone <cottonesofia@outlook.it>
2024-05-23 16:37:30 +0200

    feat: create index to print all resources + add cards with dynamic content

---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container pt-4">
+        <div class="row row-cols-4">
+            @foreach ($comics as $comic)
+                <div class="col mt-3">
+                    <div class="card">
+                        <img src="{{ $comic->thumb }}" class="card-img-top" alt="{{ $comic->title }}">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $comic->title }}</h5>
+                            <div class="card-text text-success">${{ $comic->price }}</div>
+                            <div class="card-text">Series: {{ $comic->series }}</div>
+                            <div class="card-text">Type: {{ $comic->type }}</div>
+                            <a href="#" class="btn btn-primary mt-2">More</a>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+@endsection
