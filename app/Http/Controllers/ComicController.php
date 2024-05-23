23d100a50c930e9ca15305e5c79bc505dce5be50
sofiacottone <cottonesofia@outlook.it>
2024-05-23 16:35:14 +0200

    feat: add index method to display all resources

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comic;
 use Illuminate\Http\Request;
 
 class ComicController extends Controller
@@ -13,7 +14,11 @@ class ComicController extends Controller
      */
     public function index()
     {
-        //
+        $comics = Comic::all();
+        $data = [
+            'comics' => $comics,
+        ];
+        return view('comics.index', $data);
     }
 
     /**
