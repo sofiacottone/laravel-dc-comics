a7da4bd43d08d11dcf5e7c651fa031d57838f058
sofiacottone <cottonesofia@outlook.it>
2024-05-23 17:31:11 +0200

    feat: add brand logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,9 +17,11 @@
 
 <body>
     <header>
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top">
             <div class="container-fluid">
-                <a class="navbar-brand fw-bold" aria-current="page" href="{{ route('home') }}">DC Comics</a>
+                <a class="navbar-brand fw-bold" aria-current="page" href="{{ route('home') }}">
+                    <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="DC Logo">
+                </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                     aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
