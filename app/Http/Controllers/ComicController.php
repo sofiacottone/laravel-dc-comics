6d088538945734eec71450fd23d9c2b1f0f53674
sofiacottone <cottonesofia@outlook.it>
2024-05-27 15:29:50 +0200

    feat: add destroy method to delete single resource

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -114,6 +114,9 @@ class ComicController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $comic = Comic::findOrFail($id);
+        $comic->delete();
+
+        return redirect()->route('comics.index');
     }
 }
