01fde72f1a0dd802f3f301b7a636a3b6cb4a8ba4
sofiacottone <cottonesofia@outlook.it>
2024-05-27 16:59:22 +0200

    feat: add custom modal

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -26,7 +26,8 @@
                                         @csrf
                                         @method('DELETE')
                                         <button type="submit" href="{{ route('comics.edit', ['comic' => $comic->id]) }}"
-                                            class="btn btn-outline-danger mt-2 w-100">Delete</abutton>
+                                            data-comic-title="{{ $comic->title }}"
+                                            class="js-delete-btn btn btn-outline-danger mt-2 w-100">Delete</abutton>
                                     </form>
                                 </div>
                             </div>
@@ -34,6 +35,28 @@
                     </div>
                 @endforeach
             </div>
+
+            {{-- modal  --}}
+            <div id="confirmDeleteModal" class="ms-modal">
+
+                {{-- Modal content --}}
+                <div class="ms-modal-content">
+                    <div class="hstack align-items-center justify-content-between mb-3">
+                        <div class="hstack align-items-center justify-content-center gap-2">
+                            <img src="https://img.icons8.com/ios/40/ffffff/box-important--v1.png" alt="box-important--v1" />
+                            <div class="fs-3">Confirm delete</div>
+                        </div>
+                        <div class="ms-close">&times;</div>
+                    </div>
+                    <div class="ms-modal-body"></div>
+                    <div class="hstack justify-content-end gap-2">
+                        <button class="ms-close-btn btn btn-secondary">Cancel</button>
+                        <button id="modal-confirm" class="btn btn-danger">Delete</button>
+                    </div>
+                </div>
+
+            </div>
+
         </div>
     </div>
 @endsection
