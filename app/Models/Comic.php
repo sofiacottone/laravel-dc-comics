a40e66e7076615a5a62f9dd7e385f0d15a70d4f7
sofiacottone <cottonesofia@outlook.it>
2024-05-27 18:50:24 +0200

    feat: add soft delete

--- app/Models/Comic.php
+++ app/Models/Comic.php
@@ -4,9 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Comic extends Model
 {
     use HasFactory;
+    use SoftDeletes;
     protected $fillable = ['title', 'description', 'thumb', 'price', 'series', 'sale_date', 'type'];
 }
