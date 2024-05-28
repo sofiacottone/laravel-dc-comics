85dcb3c72620e99c047936bc1ed8398ab23ab5cc
sofiacottone <cottonesofia@outlook.it>
2024-05-28 15:23:10 +0200

    feat: add error message in case of failed validation

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -7,33 +7,61 @@
             @csrf
             <div class="mb-3">
                 <label for="title" class="form-label">Title</label>
-                <input type="text" class="form-control" id="title" name="title">
+                <input type="text" class="form-control" id="title" name="title" value="{{ old('title') }}">
+                @error('title')
+                    <div class="text-danger ps-2 fw-bold">{{ $message }}</div>
+                @enderror
             </div>
+
             <div class="mb-3">
                 <label for="price" class="form-label">Price</label>
-                <input type="text" class="form-control" id="price" name="price">
+                <input type="text" class="form-control" id="price" name="price" value="{{ old('price') }}">
+                @error('price')
+                    <div class="text-danger ps-2 fw-bold">{{ $message }}</div>
+                @enderror
             </div>
+
             <div class="mb-3">
                 <label for="series" class="form-label">Series</label>
-                <input type="text" class="form-control" id="series" name="series">
+                <input type="text" class="form-control" id="series" name="series" value="{{ old('series') }}">
+                @error('series')
+                    <div class="text-danger ps-2 fw-bold">{{ $message }}</div>
+                @enderror
             </div>
+
             <div class="mb-3">
                 <label for="sale-date" class="form-label">Sale date (yyyy-mm-dd)</label>
-                <input type="text" class="form-control" id="sale-date" name="sale_date">
+                <input type="text" class="form-control" id="sale-date" name="sale_date" value="{{ old('sale_date') }}">
+                @error('sale_date')
+                    <div class="text-danger ps-2 fw-bold">{{ $message }}</div>
+                @enderror
             </div>
+
             <div class="mb-3">
                 <label for="type" class="form-label">Type</label>
-                <input type="text" class="form-control" id="type" name="type">
+                <input type="text" class="form-control" id="type" name="type" value="{{ old('type') }}">
+                @error('type')
+                    <div class="text-danger ps-2 fw-bold">{{ $message }}</div>
+                @enderror
             </div>
+
             <div class="mb-3">
                 <label for="thumb" class="form-label">Image Url</label>
-                <input type="text" class="form-control" id="thumb" name="thumb">
+                <input type="text" class="form-control" id="thumb" name="thumb" value="{{ old('thumb') }}">
+                @error('thumb')
+                    <div class="text-danger ps-2 fw-bold">{{ $message }}</div>
+                @enderror
             </div>
+
             <div class="mb-3">
                 <label for="description" class="form-label">Description</label>
-                <textarea name="description" id="description" rows="10" class="form-control"></textarea>
+                <textarea name="description" id="description" rows="10" class="form-control">{{ old('description') }}</textarea>
+                @error('description')
+                    <div class="text-danger ps-2 fw-bold">{{ $message }}</div>
+                @enderror
             </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
+
+            <button type="submit" class="btn btn-primary">Add</button>
         </form>
     </div>
 @endsection
--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -9,32 +9,66 @@
 
             <div class="mb-3">
                 <label for="title" class="form-label">Title</label>
-                <input type="text" class="form-control" id="title" name="title" value="{{ $comic->title }}">
+                <input type="text" class="form-control" id="title" name="title"
+                    value="{{ old('title', $comic->title) }}">
+                @error('title')
+                    <div class="text-danger ps-2 fw-bold">{{ $message }}</div>
+                @enderror
             </div>
+
             <div class="mb-3">
                 <label for="price" class="form-label">Price</label>
-                <input type="text" class="form-control" id="price" name="price" value="{{ $comic->price }}">
+                <input type="text" class="form-control" id="price" name="price"
+                    value="{{ old('price', $comic->price) }}">
+                @error('price')
+                    <div class="text-danger ps-2 fw-bold">{{ $message }}</div>
+                @enderror
             </div>
+
             <div class="mb-3">
                 <label for="series" class="form-label">Series</label>
-                <input type="text" class="form-control" id="series" name="series" value="{{ $comic->series }}">
+                <input type="text" class="form-control" id="series" name="series"
+                    value="{{ old('series', $comic->series) }}">
+                @error('series')
+                    <div class="text-danger ps-2 fw-bold">{{ $message }}</div>
+                @enderror
             </div>
+
             <div class="mb-3">
                 <label for="sale-date" class="form-label">Sale date (yyyy-mm-dd)</label>
-                <input type="text" class="form-control" id="sale-date" name="sale_date" value="{{ $comic->sale_date }}">
+                <input type="text" class="form-control" id="sale-date" name="sale_date"
+                    value="{{ old('sale_date', $comic->sale_date) }}">
+                @error('sale_date')
+                    <div class="text-danger ps-2 fw-bold">{{ $message }}</div>
+                @enderror
             </div>
+
             <div class="mb-3">
                 <label for="type" class="form-label">Type</label>
-                <input type="text" class="form-control" id="type" name="type" value="{{ $comic->type }}">
+                <input type="text" class="form-control" id="type" name="type"
+                    value="{{ old('type', $comic->type) }}">
+                @error('type')
+                    <div class="text-danger ps-2 fw-bold">{{ $message }}</div>
+                @enderror
             </div>
+
             <div class="mb-3">
                 <label for="thumb" class="form-label">Image Url</label>
-                <input type="text" class="form-control" id="thumb" name="thumb" value="{{ $comic->thumb }}">
+                <input type="text" class="form-control" id="thumb" name="thumb"
+                    value="{{ old('thumb', $comic->thumb) }}">
+                @error('thumb')
+                    <div class="text-danger ps-2 fw-bold">{{ $message }}</div>
+                @enderror
             </div>
+
             <div class="mb-3">
                 <label for="description" class="form-label">Description</label>
-                <textarea name="description" id="description" rows="10" class="form-control">{{ $comic->description }}</textarea>
+                <textarea name="description" id="description" rows="10" class="form-control">{{ old('description', $comic->description) }}</textarea>
+                @error('description')
+                    <div class="text-danger ps-2 fw-bold">{{ $message }}</div>
+                @enderror
             </div>
+
 
             <div class="hstack gap-2">
                 <a href="{{ route('comics.show', ['comic' => $comic->id]) }}" class="btn btn-secondary">Back</a>
